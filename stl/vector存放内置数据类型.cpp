b8d7d7073015bb2453951d10c2643f0c98137716
njylll <18962893126@163.com>
2021-06-09 21:52:45 +0800

    三种遍历方式

--- stl/vector存放内置数据类型.cpp
+++ stl/vector存放内置数据类型.cpp
@@ -1,1 +1,48 @@
-#include
+#include <iostream>
+#include <vector>
+#include <algorithm>//标准算法头文件
+using namespace std;
+
+void myPrint(int val)
+{
+    cout << val << endl;
+}
+
+void test()
+{
+    vector<int> v;
+
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    v.push_back(40);
+    //通过迭代器访问容器中的数据
+    // vector<int>::iterator itBegin = v.begin();//起始迭代器 指向容器中的第一个元素
+    // vector<int>::iterator itEnd = v.end();//结束迭代器，指向容器中最后一个元素的下一个位置
+
+    //第一种遍历方式
+    // while(itBegin != itEnd)
+    // {
+    //     cout << *itBegin << endl;
+    //     itBegin++;
+    // }
+
+    //第二种遍历方式
+    // for(vector<int>::iterator it = v.begin(); it != v.end(); it++)
+    // {
+    //     cout << *it << endl;
+    // }
+
+    //第三种遍历方式 利用STL提供的遍历算法
+    for_each(v.begin(),v.end(),myPrint);
+
+
+    
+}
+
+int main()
+{
+    test();
+    system("pause");
+    return 0;
+}

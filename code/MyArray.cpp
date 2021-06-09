4f5f557fe81cb9367134bccf9bd75f184de0053b
18962893126 <18962893126@163.com>
2021-06-09 17:22:42 +0800

    MyArray部分实现（1）

--- code/MyArray.cpp
+++ code/MyArray.cpp
@@ -50,6 +50,43 @@ public:
         return *this;
     }
 
+    //尾插法
+    void Push_Back(const T& val)
+    {
+        if(this->m_Capacity == this->m_Size)
+        {
+            cout << "已满" << endl;
+            return;
+        }
+        this->pAddress[this->m_Size] = val;
+        this->m_Size++;
+    }
+    //尾删法:让用户访问不到最后一个元素
+    void Pop_Back()
+    {
+        if(this->m_Size == 0)
+        {
+            return;
+        }
+        this->m_Size--;
+    }
+
+    //通过下标访问元素
+    T& operator[](int index)
+    {
+        return this->pAddress[index];
+    }
+
+    int getCapacity()
+    {
+        return this->m_Capacity;
+    }
+
+    int size()
+    {
+        return this->m_Size;
+    }
+
     ~MyArray()
     {
         cout << "析构函数调用" << endl; 
@@ -68,9 +105,11 @@ private:
 void test01()
 {
     MyArray<int> arr1(5);
-    MyArray<int> arr2(arr1);
-    MyArray<int> arr3(100);
-    arr3 = arr1;
+    arr1.Push_Back(1);
+    cout << "arr1[1] = " << arr1[1] << endl;
+   // MyArray<int> arr2(arr1);
+    //MyArray<int> arr3(100);
+    //arr3 = arr1;
 }
 
 int main()

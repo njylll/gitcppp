d74e2c09aa146db02782e711d99102ffbf6d76d8
18962893126 <18962893126@163.com>
2021-06-15 11:47:01 +0800

    deque

--- deque/构造函数.cpp
+++ deque/构造函数.cpp
@@ -18,20 +18,16 @@ deque<int> d1;
 for(int i = 0;i < 10;i++)
 
 {
-
-d1.push_back(i);
-
+    d1.push_back(i);
 }
 
-
-deque<int>::iterator it = d1.begin();
-
-// cout << *(d1.begin()+1);
-
-deque<int> d2(d1.begin(),d1.begin()+3); 
-
+deque<int> d2(4,100);
 printDeq(d2);
 
+
+deque<int> d3(d1.begin(),d1.begin()+3); 
+
+printDeq(d3);
 
 }
 
